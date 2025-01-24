df0a9fe30379c3213b60bbac1bc77445c9d5cbe1
b0papzso <b0papzso@vasvari.org>
2025-01-24 09:28:32 +0100

    vannak termékek és ki vannak írva

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+  import axios from "axios";
+  import {ref, onMounted} from 'vue'
+  const shoppingList = ref([])
+
+  onMounted(() =>{
+    axios.get("http://localhost:3000/shoppinglist")
+    .then(resp => {
+      console.log(resp.data)
+      shoppingList.value = resp.data
+  })
+  })
+</script>
+
+<template>
+  <h1 class="text-center">Bevásárlólista</h1>  
+  <div class="d-flex container">
+  <div v-for="item in shoppingList" class="d-flex flex-column container justify-content-between">
+    <div class="card mt-3" style="width: 18rem;">
+      <div class="card-body">
+    <h5 class="card-title">{{ item.id }}</h5>
+    <p class="card-text" >Tárgy: {{ item.item}}</p>
+    <p class="card-text">Kiszerelés: {{ item.packaging }}</p>  </div>
+</div>
+</div>
+  </div>
+</template>
