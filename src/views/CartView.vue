f1369ad0576150e4bed5ec1c36f985446aea5eda
b0papzso <b0papzso@vasvari.org>
2025-02-14 09:34:47 +0100

    tv megfelelő ár

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -8,17 +8,21 @@ const boltStore = useBoltStore()
     <p v-if="Object.entries(boltStore.cart).length < 1">籃子是空的</p>
     <div v-else>
       <table class="table table-striped col-md-9">
+        <thead>
         <th>Termék</th>
         <th>Mennyiség</th>
         <th>Ár</th>
         <th>Összesen</th>
+      </thead>
+      <tbody>
         <tr v-for="(v, k) in boltStore.cart"> 
         <td>{{ boltStore.products.find(p => p.id == k).name}}: </td>
         <td>{{ v }} db</td>
         <td>{{ boltStore.products.find(p => p.id == k).price }} / db</td>
         <td>{{ v * parseFloat(boltStore.products.find(p => p.id == k).price) }} Ft</td>
       </tr>
-      <tr colspan="4"><strong>Végösszeg:</strong><td>{{ boltStore.countTotal() }} Ft</td></tr>
+      <tr class="fs-5" colspan="4"><strong>Végösszeg:</strong><td>{{ boltStore.countTotal() }} Ft</td></tr>
+    </tbody>
     </table>
       <button @click="boltStore.emptyCart()" class="btn btn-outline-danger">Kosár ürítése</button>
     </div>
